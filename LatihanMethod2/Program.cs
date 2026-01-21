8989951a44b18b5e6faab7a7b568dc7652629c36
belfa401-bit <flbelfa401@gmail.com>
2026-01-21 15:04:29 +0700

    LaMed 2

--- LatihanMethod2/Program.cs
+++ LatihanMethod2/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int jumlahitem, hargaitem;
+
+decimal diskon, totalbayar;
+
+decimal hitungdiskon(int jml_item)
+{
+    if (jml_item == 5)
+        return 50000;
+    else if (jml_item == 10)
+        return 75000;
+    else
+        return 0;
+}
+
+decimal hitungtotalbayar(int jml_item, int harga_item , decimal diskon)
+{
+    return jml_item * harga_item - diskon;
+}
+
+Console.WriteLine("masukkan jumlah item: ");
+jumlahitem = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Masukkan Harga Item");
+hargaitem = int.Parse(Console.ReadLine());
+
+diskon = hitungdiskon(jumlahitem);
+
+totalbayar = hitungtotalbayar(jumlahitem, hargaitem, diskon);
+
+Console.WriteLine($"Total Bayar: Rp {totalbayar}");
+
